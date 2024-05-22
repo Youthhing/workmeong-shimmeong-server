f8e19194c22ba1a09dae0fc6374b241f7bb488d8
youth <boysoeng@naver.com>
2024-05-23 00:04:27 +0900

    Chore: initial commit

---
+++ src/main/java/goormthon10/workmeongshimmeong/WorkmeongShimmeongApplication.java
@@ -0,0 +1,13 @@
+package goormthon10.workmeongshimmeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmeongShimmeongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkmeongShimmeongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goormthon10/workmeongshimmeong/WorkmeongShimmeongApplicationTests.java
@@ -0,0 +1,13 @@
+package goormthon10.workmeongshimmeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmeongShimmeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
