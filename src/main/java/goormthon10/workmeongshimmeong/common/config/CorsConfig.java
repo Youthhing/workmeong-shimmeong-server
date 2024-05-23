babeb7aa589b405fe69fca590c552e1a1cbcceb1
youth <boysoeng@naver.com>
2024-05-24 05:27:49 +0900

    Update Cors method

--- src/main/java/goormthon10/workmeongshimmeong/common/config/CorsConfig.java
+++ src/main/java/goormthon10/workmeongshimmeong/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package goormthon10.workmeongshimmeong.common.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,6 +19,7 @@ public class CorsConfig {
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("POST","GET","OPTION","HEADER","PATCH"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
