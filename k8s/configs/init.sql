f9912127703f008fa3cd275b6441074880f6c0f0
youth <boysoeng@naver.com>
2024-05-24 03:25:00 +0900

    Chore: Update yaml

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -21,63 +21,68 @@ CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON `workmeong`.* TO 'root'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
-USE `workmeong`;
-
-CREATE TABLE IF NOT EXISTS `image` (
-                                       `image_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                       `image_url` VARCHAR(500) NOT NULL,
-    `image_order` INT NOT NULL,
-    `program_id` BIGINT,
-    PRIMARY KEY (`image_id`),
-    CONSTRAINT `fk_image_program`
-    FOREIGN KEY (`program_id`)
-    REFERENCES `program` (`id`)
-    ON DELETE CASCADE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `member` (
-                                        `member_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                        `member_email` VARCHAR(50) NOT NULL,
+-- member 테이블 생성
+CREATE TABLE IF NOT EXISTS `workmeong`.`member` (
+                                                    `member_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                    `member_email` VARCHAR(50) NOT NULL,
     `member_name` VARCHAR(255),
-    `member_type` VARCHAR(255) NOT NULL,
-    `member_phone` VARCHAR(255) NOT NULL,
+    `member_type` ENUM('TYPE1', 'TYPE2') NOT NULL,
+    `member_phone` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`member_id`),
-    UNIQUE INDEX `idx_member_email` (`member_email` ASC)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    UNIQUE INDEX `member_email_UNIQUE` (`member_email` ASC) VISIBLE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `program` (
-                                         `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                         `program_name` VARCHAR(255) NOT NULL,
+-- program 테이블 생성
+CREATE TABLE IF NOT EXISTS `workmeong`.`program` (
+                                                     `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                     `program_name` VARCHAR(255) NOT NULL,
     `program_description` TEXT NOT NULL,
     `program_road_name_address` VARCHAR(255) NOT NULL,
     `program_start_time` VARCHAR(255) NOT NULL,
     `program_time` BIGINT NOT NULL,
     `program_chat_link` VARCHAR(255),
-    `space_type` VARCHAR(255) NOT NULL,
-    `space_status` VARCHAR(255) NOT NULL DEFAULT 'AVAILABLE',
+    `space_type` ENUM('TYPE1', 'TYPE2') NOT NULL,
+    `space_status` ENUM('AVAILABLE', 'UNAVAILABLE') NOT NULL DEFAULT 'AVAILABLE',
     `program_money` BIGINT NOT NULL,
-    `space_number` BINARY(16) NOT NULL UNIQUE,
+    `space_number` CHAR(36) NOT NULL,
     `host_id` BIGINT,
     PRIMARY KEY (`id`),
+    UNIQUE INDEX `space_number_UNIQUE` (`space_number` ASC) VISIBLE,
+    INDEX `fk_program_member_idx` (`host_id` ASC) VISIBLE,
     CONSTRAINT `fk_program_member`
     FOREIGN KEY (`host_id`)
-    REFERENCES `member` (`member_id`)
-    ON DELETE CASCADE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    REFERENCES `workmeong`.`member` (`member_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `reservation` (
-                                             `reservation_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                             `reservation_status` VARCHAR(255) NOT NULL DEFAULT 'WAITING',
+-- image 테이블 생성
+CREATE TABLE IF NOT EXISTS `workmeong`.`image` (
+                                                   `image_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                   `image_url` VARCHAR(500) NOT NULL,
+    `image_order` INT NOT NULL,
+    `program_id` BIGINT,
+    PRIMARY KEY (`image_id`),
+    INDEX `fk_image_program_idx` (`program_id` ASC) VISIBLE,
+    CONSTRAINT `fk_image_program`
+    FOREIGN KEY (`program_id`)
+    REFERENCES `workmeong`.`program` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- reservation 테이블 생성
+CREATE TABLE IF NOT EXISTS `workmeong`.`reservation` (
+                                                         `reservation_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                         `reservation_status` ENUM('WAITING', 'CONFIRMED') NOT NULL DEFAULT 'WAITING',
     `space_id` BIGINT,
     `guest_id` BIGINT NOT NULL,
     `reservation_request` TEXT,
     PRIMARY KEY (`reservation_id`),
+    INDEX `fk_reservation_program_idx` (`space_id` ASC) VISIBLE,
     CONSTRAINT `fk_reservation_program`
     FOREIGN KEY (`space_id`)
-    REFERENCES `program` (`id`)
-    ON DELETE CASCADE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
--- CREATE DATABASE IF NOT EXISTS workmeong;
+    REFERENCES `workmeong`.`program` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
