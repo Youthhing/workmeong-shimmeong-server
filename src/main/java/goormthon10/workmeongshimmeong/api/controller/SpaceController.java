74156b98c352e7be0f15ff5c78a14ec7863b1b7e
youth <boysoeng@naver.com>
2024-05-23 14:03:01 +0900

    chore: swagger tag 추가

--- src/main/java/goormthon10/workmeongshimmeong/api/controller/SpaceController.java
+++ src/main/java/goormthon10/workmeongshimmeong/api/controller/SpaceController.java
@@ -2,6 +2,8 @@ package goormthon10.workmeongshimmeong.api.controller;
 
 import goormthon10.workmeongshimmeong.api.dto.EnrollSpaceRequest;
 import goormthon10.workmeongshimmeong.domain.service.SpaceService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Space", description = "장소 등록 관련 API 입니다.")
 @RestController
 @RequestMapping("/api/space")
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class SpaceController {
 
     private final SpaceService spaceService;
 
+    @ApiResponse(description = "장소 추가 API")
     @PostMapping("/add")
     public ResponseEntity<?> enrollSpace(@RequestBody EnrollSpaceRequest request) {
         return ResponseEntity.ok(spaceService.enrollSpace(request));
