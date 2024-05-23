3f00c245adf0324aebc38a770c683916156ab8c8
youth <boysoeng@naver.com>
2024-05-24 08:30:27 +0900

    feat: field error handle

--- src/main/java/goormthon10/workmeongshimmeong/common/error/ErrorCode.java
+++ src/main/java/goormthon10/workmeongshimmeong/common/error/ErrorCode.java
@@ -10,10 +10,12 @@ public enum ErrorCode {
 
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "E-404", "해당 회원을 찾을 수 없습니다." ),
     CANNOT_RESERVE(HttpStatus.FORBIDDEN, "R-403", " 해당 숙소는 이미 예약 되어있습니다."),
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "I-400" , "잘못된 입력 값 입니다."),
 
     IMAGE_PROCESSING_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "I-500", "이미지 업로드 과정에서의 에러 발생"),
     IMAGE_DELETE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "I-501"," 이미지 삭제과정에서 에러 발생"),
-    SQL_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "S-011", "SQL 에러 발생");
+    SQL_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "S-011", "SQL 에러 발생"),
+    ;
     private final HttpStatus status;
     private final String code;
     private final String message;
--- src/main/java/goormthon10/workmeongshimmeong/common/error/GlobalExceptionHandler.java
+++ src/main/java/goormthon10/workmeongshimmeong/common/error/GlobalExceptionHandler.java
@@ -1,13 +1,20 @@
 package goormthon10.workmeongshimmeong.common.error;
 
+import goormthon10.workmeongshimmeong.common.error.MethodArgumentErrorResponse.FieldErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -20,6 +27,30 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("[Entity Not Found Exception 발생 ] : {}", e.getMessage());
         ErrorResponse errorResponse = ErrorResponse.of(request, ErrorCode.ENTITY_NOT_FOUND, e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        HttpServletRequest httpServletRequest = servletWebRequest.getRequest();
+        String requestURI = httpServletRequest.getRequestURI();
+
+        List<FieldErrorResponse> fieldErrorResponses = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldErrorResponse::of)
+                .toList();
+
+        List<String> errorFields = fieldErrorResponses.stream()
+                .map(FieldErrorResponse::getField)
+                .toList();
+
+        log.error("[Method Argument Not Valid Execption 발생]: {}", errorFields);
+        log.error("에러가 발생한 지점 {}, {}", httpServletRequest.getMethod(), requestURI);
+
+        MethodArgumentErrorResponse errorResponse = MethodArgumentErrorResponse.of(
+                ErrorCode.INVALID_INPUT, httpServletRequest, fieldErrorResponses);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(BusinessException.class)
